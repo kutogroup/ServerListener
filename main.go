1cfeecba4317136ed53a4e7b264fc09f1c6ecb72
Simon Sun <sun.zg@outlook.com>
2019-07-08 10:06:11 +0800

    update

--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
+	"ServerListener/models"
 	m "ServerListener/models"
-	"kuto/config"
+	"fmt"
 	"kuto/pkg"
 	"kuto/utils"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/go-chi/chi"
 )
 
 var ticks = 0
-
-var h = NewHTTP(
-	nil,
-	pkg.NewDatabase("sl", "localhost:3306", "root", "root"),
-	pkg.NewLogger(os.Stdout, true),
-	pkg.NewCache(config.CacheHost, config.CachePoolSize, time.Minute))
+var db = pkg.NewDatabase("sl", "localhost:3306", "root", "root")
+var logger = pkg.NewLogger(os.Stdout, true)
 
 func main() {
 	var servers []m.Server
-	err := h.DB.Select(&servers, "id>0")
+	err := db.Select(&servers, "id>0")
 	if err != nil {
 		panic(err)
 	}
@@ -44,47 +44,47 @@ func main() {
 
 			if ticks%5 == 0 {
 				for _, s := range servers {
-					h.Logger.I("start get server, username=%s, host=%s", s.Username, s.Host)
+					logger.I("start get server, username=%s, host=%s", s.Username, s.Host)
 					sri, err := strconv.ParseInt(s.ReceiverStart, 10, 64)
 					if err != nil {
-						h.Logger.E("get server receive err=%s", err)
+						logger.E("get server receive err=%s", err)
 						continue
 					}
 
 					sti, err := strconv.ParseInt(s.TransmitStart, 10, 64)
 					if err != nil {
-						h.Logger.E("get server transmit err=%s", err)
+						logger.E("get server transmit err=%s", err)
 						continue
 					}
 
 					r := strings.TrimRight(utils.CommandGetResult("./receive", s.Username, s.Host), "\n")
 					t := strings.TrimRight(utils.CommandGetResult("./transmit", s.Username, s.Host), "\n")
-					h.Logger.I("r=%s, t=%s", r, t)
+					logger.I("r=%s, t=%s", r, t)
 
 					speed := &m.Speed{}
 					ri, err := strconv.ParseInt(r, 10, 64)
 					if err != nil {
-						h.Logger.E("get receive err=%s", err)
+						logger.E("get receive err=%s", err)
 						continue
 					}
 
 					ti, err := strconv.ParseInt(t, 10, 64)
 					if err != nil {
-						h.Logger.E("get transmit err=%s", err)
+						logger.E("get transmit err=%s", err)
 						continue
 					}
 
 					speed.ServerID = s.ID
 					speed.Receive = strconv.FormatInt(ri-sri, 10)
 					speed.Transmit = strconv.FormatInt(ti-sti, 10)
-					err = h.DB.Insert(speed)
+					err = db.Insert(speed)
 					if err != nil {
-						h.Logger.E("insert db failed, err=%s", err)
+						logger.E("insert db failed, err=%s", err)
 						continue
 					}
 
 					c := utils.CommandGetResult("./conn", s.Username, s.Host, strconv.FormatInt(s.Port, 10))
-					h.Logger.I("c=%s", c)
+					logger.I("c=%s", c)
 					conn := &m.Conns{}
 					num, err := strconv.ParseInt(c, 10, 64)
 					if err != nil {
@@ -92,47 +92,81 @@ func main() {
 					}
 					conn.Conns = num
 					conn.ServerID = s.ID
-					h.DB.Insert(conn)
+					db.Insert(conn)
 				}
 			}
 		}
 	}()
 
-	h.Serve(":80", time.Minute)
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Not available"))
+	})
+	r.Get("/netstat", NetStat)
+	log.Fatal(http.ListenAndServe(":80", r))
 }
 
 func initServer(servers []m.Server) {
 	for _, s := range servers {
 		r := strings.TrimRight(utils.CommandGetResult("./receive", s.Username, s.Host), "\n")
 		t := strings.TrimRight(utils.CommandGetResult("./transmit", s.Username, s.Host), "\n")
-		h.Logger.I("r=%s, t=%s", r, t)
+		logger.I("r=%s, t=%s", r, t)
 
 		ri, err := strconv.ParseInt(r, 10, 64)
 		if err != nil {
-			h.Logger.E("init server receive err=%s", err)
+			logger.E("init server receive err=%s", err)
 			continue
 		}
 
 		ti, err := strconv.ParseInt(t, 10, 64)
 		if err != nil {
-			h.Logger.E("init server transmit err=%s", err)
+			logger.E("init server transmit err=%s", err)
 			continue
 		}
 
 		s.ReceiverStart = strconv.FormatInt(ri, 10)
 		s.TransmitStart = strconv.FormatInt(ti, 10)
-		err = h.DB.Update(&s, m.ColumnServerReceiverStart, m.ColumnServerTransmitStart)
+		err = db.Update(&s, m.ColumnServerReceiverStart, m.ColumnServerTransmitStart)
 		if err != nil {
-			h.Logger.E("update db failed, err=%s", err)
+			logger.E("update db failed, err=%s", err)
 		}
 	}
 }
 
-//Index 首页
-func (c *HTTP) Index(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Not available"))
-}
+//流量监听
+func NetStat(w http.ResponseWriter, r *http.Request) {
+	y, m, d := time.Now().Date()
 
-//NetStatus 网络状态
-func (c *HTTP) NetStatus(w http.ResponseWriter, r *http.Request) {
+	var servers []models.Server
+
+	r.ParseForm()
+	cs := r.Form.Get("startDate")
+	ce := r.Form.Get("endDate")
+
+	if len(cs) != 10 {
+		cs = fmt.Sprintf("%04d-%02d-%02d", y, m, d)
+	}
+
+	if len(ce) != 10 {
+		ce = fmt.Sprintf("%04d-%02d-%02d 23:59:59", y, m, d)
+	} else {
+		ce = ce + " 23:59:59"
+	}
+	fmt.Println("cs=" + cs + ", ce=" + ce)
+
+	err := db.Select(&servers,
+		fmt.Sprintf("%s>='%s' AND %s<='%s'",
+			models.ColumnServerCreateAt, cs,
+			models.ColumnServerCreateAt, ce))
+
+	if err != nil {
+		logger.E("select today failed, err=%s", err)
+		w.WriteHeader(403)
+		return
+	}
+
+	startDate := chi.URLParam(r, "startDate")
+	endDate := chi.URLParam(r, "endDate")
+
+	w.Write([]byte(startDate + "</br>" + endDate))
 }
